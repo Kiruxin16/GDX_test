648724285e1eaa167a1e584deeebb7e01951c1b0
Kirill <kiruxin16@gmail.com>
2022-09-11 21:36:25 +0700

    Ad tileset

---
+++ assets/tilesets/Map_Snow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="Map_Snow" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="15" columns="15">
+ <image source="Map_Snow.png" width="512" height="64"/>
+</tileset>
