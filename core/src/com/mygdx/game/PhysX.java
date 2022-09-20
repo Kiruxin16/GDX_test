b1efec90e8cf487ea223be6a2bceb546774d0dab
Kirill <kiruxin16@gmail.com>
2022-09-20 20:12:47 +0700

    Work with physics

---
+++ core/src/com/mygdx/game/PhysX.java
@@ -0,0 +1,66 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+public class PhysX {
+    private final World world;
+    private final Box2DDebugRenderer debugRenderer;
+
+
+    public PhysX() {
+        world = new World(new Vector2(0,-9.81f),true);
+        this.debugRenderer = new Box2DDebugRenderer();
+    }
+
+    public void  setGravity(Vector2 gravity){
+        world.setGravity(gravity);
+    }
+
+    public void dispose(){
+        world.dispose();
+        debugRenderer.dispose();
+
+    }
+    public void step(){
+        world.step(1/60f,3,3);
+    }
+    public void debugDraw(OrthographicCamera camera){
+        debugRenderer.render(world,camera.combined);
+    }
+
+
+    public Body addObject(RectangleMapObject recMO){
+        Rectangle rect = recMO.getRectangle();
+        String type = (String) recMO.getProperties().get("BodyType");
+        BodyDef def = new BodyDef();
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape polygonShape = new PolygonShape();
+
+        switch (type){
+            case "DynamicBody":def.type = BodyDef.BodyType.DynamicBody;break;
+            default:def.type = BodyDef.BodyType.StaticBody;break;
+        }
+
+
+        def.position.set((rect.x +rect.width/2)/10,(rect.y+rect.height/2)/10);
+        def.gravityScale =10;
+        polygonShape.setAsBox(rect.width/20,rect.height/20);
+        fixtureDef.shape =polygonShape;
+        fixtureDef.friction = 1;
+        fixtureDef.density=0f;
+        fixtureDef.restitution=0.01f;
+
+        Body body =world.createBody(def);
+        body.createFixture(fixtureDef).setUserData("ground");
+
+        polygonShape.dispose();
+        return body;
+
+
+    }
+}
+
